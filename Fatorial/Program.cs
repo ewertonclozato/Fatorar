bc125f8f6a579c3ae2b8daae12092639aae2bccf
Ewerton Clozato <ewerton.clozato@xpi.com.br>
2022-01-18 12:52:10 -0300

    Fatorar

---
+++ Fatorial/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número para fatorar:");
+            var numFat = new NumeroFat(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine();
+
+            Console.WriteLine($"Fatorial de: {numFat.Numero}\n");
+            var fatorial = numFat.Numero;
+            var result = numFat.Numero;
+
+
+            for (int i = numFat.Numero - 1; i > 0; i--)
+            {
+                result *= i;
+                Console.WriteLine($"{fatorial} x {i} = {result}");
+                fatorial *= i;
+            }
+
+            Console.WriteLine();
+            Console.Write($"Fatorial de {numFat.Numero} é: {numFat.MostrarResuldado(result.ToString())}\n");
+        }
+
+        public class NumeroFat
+        {
+            public NumeroFat(int numero)
+            {
+                this.Numero = numero;
+            }
+            public int Numero { get; set; }
+            public string Resultado { get; set; }
+
+            public string MostrarResuldado(string result)
+            {
+                this.Resultado = result;
+                return Resultado;
+            }
+        }
+    }
+}
